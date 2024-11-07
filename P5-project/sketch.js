43a75b58c9079e90ae7498b37d62167189bb3cef
caiyi121 <cyan0021@uni.sydney.au>
2024-11-07 14:19:54 +0800

    pigeon
    1

--- P5-project/sketch.js
+++ P5-project/sketch.js
@@ -1,147 +1,58 @@
-//We need a variable to hold our image
-let img;
+let cloudImage;
+let birdMask;
 
-//We will divide the image into segments
-let numSegments = 50;
-
-//We will store the segments in an array
-let segments = [];
-
-//lets add a variable to switch between drawing the image and the segments
-// now i will adapt the segment so i will make them visible at start pooint.
-let drawSegments = true;
-
-// //  this an object that will hold our mona [Object D]
-let imgDrwPrps = {aspect:0 , width:0, height:0, xOffset:0, yOffset:0};
-
-// // variable for the aspect ratio of the canvas (B)
-let canvasAspectRatio = 0;
-
-//lets load the image from disk
 function preload() {
-  img = loadImage('assets/Mona_Lisa_by_Leonardo_da_Vinci_500_x_700.jpg');
+  cloudImage = loadImage('./images/clouds.jpg'); // Load the cloud image
 }
 
 function setup() {
-  //We will make the canvas the same size as the image using its properties
-  // // replace with window properties (explorer, or chrome window)
-  createCanvas(windowWidth, windowHeight);
-  // // aspect ratio of object D
-  imgDrwPrps.aspect = img.width / img.height;
-  // // aspect ratio of the canvas (replace value of variable)
-  canvasAspectRatio = width / height;
-  // ////  now we add the function and make it useful 
-  // calculateImageDrawProps();
-  //
-  //We can use the width and height of the image to calculate the size of each segment
-  let segmentWidth = img.width / numSegments;
-  let segmentHeight = img.height / numSegments;
-  /*
-  Divide the original image into segments, we are going to use nested loops
-  */
-let positionInColumn = 0;
-  for (let segYPos=0; segYPos<img.height; segYPos+=segmentHeight) {
-    let positionInRow = 0;
-    //this is looping over the height
-    for (let segXPos=0; segXPos<img.width; segXPos+=segmentWidth) {
-      //We will use the x and y position to get the colour of the pixel from the image
-      //lets take it from the centre of the segment
-      let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
-       let segment = new ImageSegment(positionInColumn,positionInRow,segmentColour);
-       segments.push(segment);
-       positionInRow++;
-    }
-    positionInColumn++;
-  }
-  for (const segment of segments) {
-    segment.calculateSegDrawProps () ;
-  }
+    createCanvas(500, 400);
+
+  // Create a graphics object to draw the bird shape for masking
+  birdMask = createGraphics(width, height);
+
+  drawDove(birdMask); // Draw the bird shape on the mask
+  cloudImage.mask(birdMask); // Apply the bird shape as a mask on the cloud image
 }
 
-function draw() {
-  background(0);
-  if (drawSegments) {
-    //lets draw the segments to the canvas
-    for (const segment of segments) {
-      //lets set the scale of each segment based on its distance from the mouse
-       segment.scale = dist(segment.srcImgSegXPos, segment.srcImgSegYPos, mouseX, mouseY)/100;
-       segment.draw();
-    }
-  } else {
-    //lets draw the image to the canvas
-    // replace values of image with the object that hold the image properties
-    image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
+  function draw() {
+    background(0, 100, 200); // Set background color
+
+    // Display the masked cloud image, so it appears in the shape of the bird
+    image(cloudImage, 0, 0, width, height);
   }
-}
 
+  function drawDove(pg) {
+    pg.fill(255); // Fill white for the mask
 
+    pg.noStroke();
 
-function keyPressed() {
-  if (key == " ") {
-    //this is a neat trick to invert a boolean variable,
-    //it will always make it the opposite of what it was
-    drawSegments = !drawSegments;
+    pg.beginShape();
+
+    // 鸽子的头部
+    pg.vertex(200, 150);
+    pg.bezierVertex(172, 141, 160, 74, 73, 3); // 上翅膀
+    pg.bezierVertex(155, 188, 109, 98, 82, 152); // 头部曲线
+    pg.bezierVertex(137, 126, 105, 144, 70, 149); // 嘴曲线
+    // 鸽子的颈部
+    pg.bezierVertex(135, 166, 98, 196, 166, 244); // 颈部曲线
+
+    // 鸽子的身体和尾部
+    pg.bezierVertex(169, 246, 238, 282, 198, 260); // 身体曲线
+    pg.bezierVertex(332, 396, 392, 292, 249, 250); // 尾部曲线
+    pg.bezierVertex(231, 200, 305, 260, 343, 160); // 尾部曲线
+
+    // 鸽子的翅膀
+    pg.bezierVertex(241, 274, 339, 195, 391, 89); // 翅膀曲线
+    pg.bezierVertex(322, 96, 487, 89, 291, 74); // 翅膀曲线
+    pg.bezierVertex(212, 57, 164, 165, 197, 155); // 翅膀回到头部
+
+    pg.endShape(CLOSE);
+
+    pg.beginShape();
+    pg.vertex(239, 296);
+    pg.vertex(294, 268);
+    pg.vertex(357, 307);
+    pg.vertex(284, 345);
+    pg.endShape(CLOSE);
   }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth,windowHeight);
-  calculateImageDrawProps() ;
-  for (const segment of segments) {
-    segment.calculateSegDrawProps();
-  }
-}
-
-//// the function that with calculate the aspect ratio required to keep the proportions of my image
-function calculateImageDrawProps() {
-  if (imgDrwPrps.aspect > canvasAspectRatio) {
-    imgDrwPrps.width = width;
-    imgDrwPrps.height = width / imgDrwPrps.aspect;
-    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
-    imgDrwPrps.xOffset = 0;
-  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
-    imgDrwPrps.height = height ;
-    imgDrwPrps.width = height * imgDrwPrps.aspect;
-    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2 ;
-    imgDrwPrps.yOffset = 0;
-  }
-  else if (imgDrwPrps.aspect == canvasAspectRatio) {
-    imgDrwPrps.width = width;
-    imgDrwPrps.height = height;
-    imgDrwPrps.xOffset=0;
-    imgDrwPrps.yOffset=0;
-  }
-}
-
-
-//Here is our class for the image segments, we start with the class keyword
-// needs to be updated to the new changing values
-class ImageSegment {
-
-  constructor(columnPositionInPrm,rowPositionInPrm,srcImgSegColourInPrm) {
-    //these parameters are used to set the internal properties of an instance of the segment
-    //These parameters are named as imageSource as they are derived from the image we are using
-    this.columnPosition = columnPositionInPrm;
-    this.rowPosition = rowPositionInPrm;
-    this.srcImgSegColour = srcImgSegColourInPrm;
-
-    this.drawXPos = 0;
-    this.drawYPos = 0;
-    this.drawWidth = 0;
-    this.drawHeight =0;
-}
- calculateSegDrawProps() {
-  this.drawWidth = imgDrwPrps.width / numSegments;
-  this.drawHeight = imgDrwPrps.height / numSegments;
-
-  this.drawXPos = this.rowPosition * this.drawWidth + imgDrwPrps.xOffset;
-  this.drawYPos = this.columnPosition * this.drawHeight + imgDrwPrps.yOffset;
- }
-
-  draw() {
-    //lets draw the segment to the canvas, now filled with the colour and with a black border
-    stroke(0);
-    fill(this.srcImgSegColour);
-    rect(this.drawXPos, this.drawYPos, this.drawWidth,this.drawHeight);
-  }
-}
